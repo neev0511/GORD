910bfee0997f6c7e101d6d38d5242cb4a74680c7
neev0511 <allofbakwas@gmail.com>
2022-10-04 00:06:27 +0530

    Bug Fixes

--- js/news.js
+++ js/news.js
@@ -1,61 +1,58 @@
 const API_KEY = "b66c02de279d403a88a0e24c6323a692";
 
+var res = [];
+
 function getNews() {
-  fetch("https://saurav.tech/NewsAPI/top-headlines/category/health/in.json")
+  fetch(
+    "https://newsapi.org/v2/everything?q='fabry diseases'&language=en&apiKey=b66c02de279d403a88a0e24c6323a692"
+  )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // This is the JSON from our response
-      console.log(data.articles);
-      news(data.articles);
-    })
-    .catch(function (err) {
-      // There was an error
-      console.log("Something went wrong.", err);
+      res.push(data.articles);
     });
 }
 
-function news(data) {
+function news(res) {
   var i = 0;
-  var res = [];
 
   document.getElementById("contain").innerHTML = "";
-  for (let j in data) {
-    if (
-      data[j].urlToImage != null &&
-      data[j].description !== "" &&
-      (data[j].urlToImage.includes(".jpg") ||
-        data[j].urlToImage.includes(".png") ||
-        data[j].urlToImage.includes(".webp"))
-    ) {
-      res.push(data[j]);
-    }
-  }
-  console.log(res);
-  while (i < parseInt(document.getElementById("news").value)) {
+  // for (let j in data) {
+  //   if (
+  //     data[j].urlToImage != null &&
+  //     data[j].description !== "" &&
+  //     (data[j].urlToImage.includes(".jpg") ||
+  //       data[j].urlToImage.includes(".png") ||
+  //       data[j].urlToImage.includes(".webp"))
+  //   ) {
+  //     res.push(data[j]);
+  //   }
+  // }
+  while (i < 15) {
+    console.log(res[i]);
     $("#contain").append(
       `
-        <a class="white" href="` +
+          <a class="white" href="` +
         res[i].url +
         `">
-            <div class="card">
-                <div class="card__header">
-                <img src="` +
+              <div class="card">
+                  <div class="card__header">
+                  <img src="` +
         res[i].urlToImage +
         `" alt="card__image" class="card__image" width="600" />
-                </div>
-                <div class="card__body">
-                <h4>` +
+                  </div>
+                  <div class="card__body">
+                  <h4>` +
         res[i].title +
         `</h4>
-                <p>` +
+                  <p>` +
         res[i].description +
         `</p>
-                </div>
-            </div>
-        </a>
-    `
+                  </div>
+              </div>
+          </a>
+      `
     );
     i++;
   }
